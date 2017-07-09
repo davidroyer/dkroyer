d83290dbe634b64ec87498ffe00e320b44b4f955
David Royer <droyer01@gmail.com>
2017-07-09 14:59:26 -0400

    Added shadow to headshot

--- pages/About.vue
+++ pages/About.vue
@@ -4,7 +4,7 @@
       <h1 class="title">
         About Me
       </h1>
-      <img class="headshot" src="~static/images/headshot.jpg" alt="">
+      <img class="headshot has-shadow" src="~static/images/headshot.jpg" alt="">
       <p>I love learning new things, growing as a person, and being creative.</p>
       <p>As a math major, I always loved problem solving, and now enjoy the opprotunity to apply this characteristic to Web Design & Development. It’s an extraordinary feeling to push through the many iterations of creation to finally realize “That is it…that’s what I want."</p>
     </div>
