1ccfac50b1fb87e6a96368279d28426de45aac2d
David Royer <droyer01@gmail.com>
2017-07-09 07:23:33 -0400

    continued testing

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@ import axios from '~plugins/axios'
 export default {
   async asyncData({params}) {
     let slug = params.slug
-    const {data} = await axios.get(`/api/posts.json`)
+    const {data} = await axios.get(`https://www.davidroyer.me/api/posts.json/api/posts.json`)
 
     return {
       posts: data
