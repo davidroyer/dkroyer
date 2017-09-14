7ce31fd615913fdb826ab362be67261bcebd678d
David Royer <droyer01@gmail.com>
2017-09-14 01:40:05 -0400

    Push headshot in assets so it can be handled

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-// import axios from './plugins/axios.js'
 const axios = require('axios')
 const postsObject = require('./static/api/posts.json')
 
@@ -14,10 +13,6 @@ function createRoutes(posts)  {
 }
 
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  // mode: 'spa',
   head: {
     title: 'David Royer - Front End Web Developer',
     titleTemplate: '%s - David Royer - Front End Web Developer',
@@ -31,18 +26,12 @@ module.exports = {
       // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto+Slab:400,700|Roboto:300,400,500,700,900|Roboto+Condensed:400,700,700i' }
     ]
   },
-  // loaders: [
-  //   {
-  //     test: /\.md$/,
-  //     loaders: ['vue-markdown-loader']
-  //   },
-  // ],
+
   plugins: [
     { src: '~/plugins/vue-resource.js', ssr: false }
   ],
   modules: [
     '@nuxtjs/bulma',
-    // '@nuxtjs/font-awesome',
     '@nuxtjs/pwa',
     ['@nuxtjs/google-analytics', { ua: 'UA-56060335-5' }],
     // '@nuxtjs/markdownit'
@@ -55,30 +44,17 @@ module.exports = {
   //     'markdown-it-front-matter'
   //   ]
   // },
-  /*
-  ** Customize the progress-bar color
-  */
   loading: { color: '#3B8070' },
   css: [
     // { src: 'bulma/css/bulma.css', lang: 'css'},
     { src: '~/assets/css/main.scss', lang: 'scss'}
   ],
-  /*
-  ** Build configuration
-  */
   env: {
     // baseUrl: process.env.BASE_URL || 'http://localhost:3000'
   },
   generate: {
     routes: function() {
       return createRoutes(postsObject)
-      // let baseURL = process.env.baseUrl
-      // return axios.get(`http://localhost:3000/api/posts.json`)
-      //
-      // .then((response) => {
-      //   let posts = response.data
-      //   return createRoutes(posts)
-      // })
     }
   },
   build: {
--- pages/About.vue
+++ pages/About.vue
@@ -9,7 +9,7 @@
       <h1 class="title">
         About Me
       </h1>
-      <img class="headshot" src="~static/images/headshot.jpg" alt="">
+      <img class="headshot" src="~/assets/images/headshot.jpg" alt="David Royer Headshot">
       <p>I love learning new things, growing as a person, and being creative.</p>
       <p>As a math major, I always loved problem solving, and now enjoy the opprotunity to apply this characteristic to Web Design & Development. It’s an extraordinary feeling to push through the many iterations of creation to finally realize “That is it…that’s what I want."</p>
     </section>
