339b54e5865bb73b3fa75f454cc2bcd0d83a13f3
David Royer <droyer01@gmail.com>
2017-09-18 06:49:06 -0400

    update  svg

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -11,7 +11,7 @@
         inline-template>
 
        <div>
-         <span id="socialShareCTA">Share</span>
+
            <!-- <network network="email">
               <i class="fa fa-envelope"></i>
            </network>
@@ -19,6 +19,7 @@
              <i class="fa fa-facebook"></i>
            </network> -->
            <network network="twitter">
+             <span id="socialShareCTA">Share</span>
              <svg id="twitterShare" viewBox="0 0 128 128">
              <path  class="cls-1" d="M40.58,115.3c47.64,0,73.69-39.47,73.69-73.69,0-1.12,0-2.24-.07-3.35a52.7,52.7,0,0,0,12.92-13.41,51.7,51.7,0,0,1-14.87,4.08A26,26,0,0,0,123.63,14.6a51.9,51.9,0,0,1-16.45,6.29A25.92,25.92,0,0,0,63.05,44.51,73.53,73.53,0,0,1,9.67,17.45a25.92,25.92,0,0,0,8,34.58A25.71,25.71,0,0,1,6,48.78c0,.11,0,.22,0,.33A25.91,25.91,0,0,0,26.73,74.5a25.86,25.86,0,0,1-11.7.44,25.93,25.93,0,0,0,24.2,18A52,52,0,0,1,7.06,104a52.72,52.72,0,0,1-6.18-.36,73.32,73.32,0,0,0,39.7,11.63" transform="translate(-0.88 -12.7)"></path>
              </svg>
@@ -84,19 +85,19 @@ export default {
     position: relative;
     display: block;
     margin-left: auto;
-    width: 140px;
+    width: 125px;
     margin-right: auto;
     margin-bottom: 4.5em;
     justify-content: space-between;
     display: flex;
-    transition: background-color .25s ease;
+    // transition: background-color .25s ease;
     cursor: pointer;
-
-    &:hover {
-      // cursor: pointer
-      color: white;
-      background: #4c4c4c;
-    }
+  padding: 0;
+    // &:hover {
+    //   // cursor: pointer
+    //   color: white;
+    //   background: #4c4c4c;
+    // }
   svg {
     stroke-width: 20px;
     height: 25px;
@@ -111,7 +112,26 @@ export default {
   span {
     margin: 0 .5em;
 
-    transition: .2s ease;
+    // transition: .2s ease;
+    &[data-link] {
+      height: inherit;
+      padding: .5em .75em;
+      width: inherit;
+      margin: 0;
+      display: flex;
+      justify-content: space-between;
+      transition: background-color .25s ease;
+
+      &:hover {
+        // cursor: pointer
+        color: white;
+        background: #4c4c4c;
+      }
+      svg {
+        position: relative;
+        top: 3px;
+      }
+    }
     &:hover {
       // color: #00d1b2;
     }
