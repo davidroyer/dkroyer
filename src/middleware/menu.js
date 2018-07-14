61fbbd490816c8063497974ef311022a45b9bd76
David Royer <droyer01@gmail.com>
2018-07-14 07:32:39 -0400

    cleaned up a bit

--- src/middleware/menu.js
+++ src/middleware/menu.js
@@ -1,17 +1,3 @@
 export default function({ isDev, store }) {
-  // let addedAlready = store.state.examplePageAdded
-  // console.log('addedAlready', addedAlready)
-  // if (isDev && !addedAlready) store.commit('addExamplePageToMenu')
   if (store.state.menuIsActive === true) store.commit('toggleMenuState')
 }
-
-// var examplesPage: { name: 'Examples', path: '/examples' };
-//
-// var navLinks = [
-//   { name: 'Home', path: '/' },
-//   { name: 'Site Info', path: '/site-info' },
-//   { name: 'Blog', path: '/blog' },
-//   { name: 'Contact', path: '/contact' },
-// ]
-// if (navLinks.includes(examplesPage)) alert("yes");
-// else alert("no");
