3c65a1cc3426837186592ec4b457383370fb187f
David Royer <droyer01@gmail.com>
2018-12-03 14:39:03 -0500

    Update website.config.js

--- website.config.js
+++ website.config.js
@@ -3,12 +3,12 @@ const { colors } = require('./tailwind.config')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
   analyticsID: 'UA-56060335-5',
-  siteTitle: 'David Royer - Front End Web Developer', // Navigation and Site Title
+  siteTitle: 'David Royer - Full Stack Developer', // Navigation and Site Title
   shortName: 'D.Royer', // Alternative Site title for SEO
   siteUrl: 'https://www.davidroyer.me', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-  siteDescription: 'Personal site of David Royer, Front-End Web Designer and Developer.',
+  siteDescription: 'Personal site of David Royer, Full Stack Developer.',
   siteFBAppID: '123456789', // Facebook App ID
   twitterUsername: '@DavidRoyer_', // Twitter Username
   ogTitle: 'David Royer',
